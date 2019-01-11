f59692df6c515bef2b373884640a7489af309c70
Sam Demeulemeester <sam.demeulemeester@vdabcampus.be>
2019-01-11 15:14:17 +0100

    Entity zoeken via primary key

---
+++ src/main/java/be/vdab/fietsacademy/repositories/JpaDocentRepository.java
@@ -0,0 +1,18 @@
+package be.vdab.fietsacademy.repositories;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+
+import be.vdab.fietsacademy.entities.Docent;
+
+class JpaDocentRepository implements DocentRepository {
+	private final EntityManager manager;
+	JpaDocentRepository(EntityManager manager) {
+		this.manager = manager;
+	}
+	@Override
+	public Optional<Docent> read(long id) {
+		return Optional.ofNullable(manager.find(Docent.class, id));
+	}	
+}
